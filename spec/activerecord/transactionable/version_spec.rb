35f2fac2f15bee1b7526b158828f05f83bf310a3
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 19:29:53 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/activerecord/transactionable/version_spec.rb
+++ spec/activerecord/transactionable/version_spec.rb
@@ -1,5 +1,5 @@
 require "anonymous_loader"
-require "activerecord-transactionable"
+require "activerecord/transactionable"
 RSpec.describe Activerecord::Transactionable::Version do
   it_behaves_like "a Version module", described_class
 
